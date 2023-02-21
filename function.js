300cb67fb0a70cd55ffebc0d185cf3cbaee10650
marianelaortiz <marianela.solange.ortiz@gmail.com>
2023-02-21 19:19:04 -0300

    Primer commit

---
+++ function.js
@@ -0,0 +1,5 @@
+console.log("Costo un poco, pero lo logre!!")
+document.getElementById('boton').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = que cosa no
+}
